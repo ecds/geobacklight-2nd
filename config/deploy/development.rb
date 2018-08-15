8281bebfde3f1dc962ea39ed2e5cd1463436b76c
SolomonHD <solomon.hilliard@emory.edu>
2018-08-14 19:00:08 -0700

    Dotenv added

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -2,6 +2,6 @@ set :branch, "master"
 role :app, %w{ec2-user@54.172.197.200}
 role :web, %w{ec2-user@54.172.197.200}
 role :db, %w{ec2-user@54.172.197.200}
-set :rails_env, "development"
+set :rails_env, "production"
 set :deploy_to, "/home/ec2-user/Test/Capdeploy"
-append :linked_files, ".env.development"
+append :linked_files, ".env.production"
