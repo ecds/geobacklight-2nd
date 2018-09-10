041bc596441ea8144ef4fc0cf647514e37fd1a79
SolomonHD <solomon.hilliard@emory.edu>
2018-09-10 08:41:34 -0700

    remove sidekiq tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,26 +21,26 @@ append :linked_dirs, "tmp/sockets"
 append :linked_files, ".env.production"
 append :linked_files, "config/secrets.yml"
 
-Rake::Task["sidekiq:stop"].clear_actions
-Rake::Task["sidekiq:start"].clear_actions
-Rake::Task["sidekiq:restart"].clear_actions
-namespace :sidekiq do
-  task :stop do
-    on roles(:app) do
-      execute :sudo, :systemctl, :stop, :sidekiq
-    end
-  end
-  task :start do
-    on roles(:app) do
-      execute :sudo, :systemctl, :start, :sidekiq
-    end
-  end
-  task :restart do
-    on roles(:app) do
-      execute :sudo, :systemctl, :restart, :sidekiq
-    end
-  end
-end
+#Rake::Task["sidekiq:stop"].clear_actions
+#Rake::Task["sidekiq:start"].clear_actions
+#Rake::Task["sidekiq:restart"].clear_actions
+#namespace :sidekiq do
+#  task :stop do
+#    on roles(:app) do
+#      execute :sudo, :systemctl, :stop, :sidekiq
+#    end
+#  end
+#  task :start do
+#    on roles(:app) do
+#      execute :sudo, :systemctl, :start, :sidekiq
+#    end
+#  end
+#  task :restart do
+#    on roles(:app) do
+#      execute :sudo, :systemctl, :restart, :sidekiq
+#    end
+#  end
+#end
 
 # After the code has been deployed, run db:seed
 # This creates the default admin user
